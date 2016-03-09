ce26044f03cb6d88be1d5841a31a5e50ff84bec1
Jackson Jacob <jjair@eri-172-38.d.port.ac.uk>
2016-03-09 16:13:32 +0000

    Home page converted to PHP

---
+++ inc:header.php/header.php
@@ -0,0 +1,22 @@
+<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Project Chart</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+
+  <div class="navbar">
+    <h1>Select a Project</h1>
+    <ul>
+      <li><a class="container" href="#">About</a></li>
+      <li><a class="container" href="#">WBT</a></li>
+      <li><a class="container" href="#">Pert</a></li>
+      <li><a class="container" href="#">Gantt</a></li>
+    </ul>
+  </div>
+
+?>
--- index.php
+++ index.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-  <title>Project Chart</title>
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
-</head>
+include('inc/header.php');
 
-<body>
-
-  <div class="navbar">
-    <h1>Select a Project</h1>
-    <ul>
-      <li><a class="container" href="#">About</a></li>
-      <li><a class="container" href="#">WBT</a></li>
-      <li><a class="container" href="#">Pert</a></li>
-      <li><a class="container" href="#">Gantt</a></li>
-    </ul>
-  </div>
+?>
 
   <div class="mainBody">
     <div class="container">
